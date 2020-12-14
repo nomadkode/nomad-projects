ac1f8e0e6c051f146d8592d34152bbeb88c2db9c
nomadkode <setiawanramadyan@gmail.com>
2020-12-14 15:19:30 +0700

    countdown timer

---
+++ countdown-timer/script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
+
+const newYears = '1 Jan 2021';
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const total = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(total / 3600 / 24);
+  const hours = Math.floor(total / 3600) % 24;
+  const minutes = Math.floor(total / 60) % 60;
+  const seconds = Math.floor(total) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
